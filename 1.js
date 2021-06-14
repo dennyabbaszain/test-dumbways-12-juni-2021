1169d7021fc75797c2d6c9d0f2736f57c36ae926
Denny Abbas Zain <55909019+dennyabbaszain@users.noreply.github.com>
2021-06-14 20:36:53 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,3 @@
-// Di suatu penangkaran terdapat 6.969 burung lovebird. Setiap 21 hari burung tersebut serempak menetaskan satu lovebird baru. Namun sebelum melahirkan, 11,1% dari lovebird tersebut mati. Buatkan lah sebuah program untuk menghitung dan mengetahui jumlah lovebird setelah 441 hari.
 const loveBird = () => {
   let lovebird = 6969;
   const jumlahMenetas = 441 / 21;
